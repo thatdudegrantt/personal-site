263934e7c03fd20698c737cd3ad9443afef77756
thatdudegrantt <polazzo.grant@gmail.com>
2025-08-04 17:29:57 -0400

    updated dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+
+    headers: async () => [
+        {
+            source: "/(.*)",
+            headers: [
+                {
+                    key: "X-Frame-Options",
+                    value: "DENY",
+                },
+                {
+                    key: "X-Content-Type-Options",
+                    value: "nosniff",
+                },
+            ],
+        },
+    ],
+    images: {
+        domains: [],
+    },
+};
 
 export default nextConfig;
