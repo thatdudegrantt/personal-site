63e3dab795d5801b18734ab6133911a584d2c3a4
thatdudegrantt <polazzo.grant@gmail.com>
2025-08-10 07:06:08 -0400

    house icon in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,8 +20,21 @@ export default function Navbar() {
         <div className="navbar bg-base-100 shadow-lg">
 
             <div className="flex-1 flex items-center gap-2">
-                <a href="/" className="btn btn-ghost normal-case text-xl text-base-content">
-                    Home
+                <a href="/" className="btn btn-ghost normal-case text-xl text-base-content" aria-label={"Home"}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                        <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                    </svg>
                 </a>
                 <ul className="menu menu-horizontal px-1 text-base-content gap-1">
                     <li>
